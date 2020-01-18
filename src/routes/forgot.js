fc6b2b1ad215221b9b242b3b916e868948c6bbdb
Anargu <anthony.arostegui@gmail.com>
2020-01-17 20:27:28 -0500

    adding validations

--- src/routes/forgot.js
+++ src/routes/forgot.js
@@ -1,15 +1,26 @@
 
 const path = require('path')
 const express = require('express')
-const { check, body, validationResult, query } = require('express-validator');
+const { check, oneOf, validationResult, query } = require('express-validator');
 const miauthConfig = require('../config')
 const { MiauthError } = require('../utils/error.js')
 
 const forgotRoute = express.Router()
 
 // step 1: user request for reset password
-forgotRoute.post('/request', () => {
+forgotRoute.post('/request', [
+    check('username', miauthConfig.field_validations.username.invalid_pattern_error_message)
+    .exists({ checkNull: true })
+    .matches(new RegExp(miauthConfig.field_validations.username.pattern, 'g'))
+    .isLength({
+        min: miauthConfig.field_validations.username.len[0],
+        max: miauthConfig.field_validations.username.len[1]
+    }).withMessage(`Invalid username. Username should be between\ 
+    ${miauthConfig.field_validations.username.len[0]} and\ 
+    ${miauthConfig.field_validations.username.len[1]} characteres`)
+], () => {
 
+    // 
 })
 
 forgotRoute.get('/reset', (req, res) => {
@@ -21,10 +32,7 @@ forgotRoute.get('/reset', (req, res) => {
 forgotRoute.post('/reset', [
     query('token', 'Token is not setted')
         .exists({ checkNull: true }),
-    check(['new_password'],
-        `password must be between\ 
-        ${miauthConfig.field_validations.password.len[0]} \ 
-        and ${miauthConfig.field_validations.password.len[1]} characters`)
+    check(['new_password'], miauthConfig.field_validations.password.invalid_pattern_error_message)
         .exists({ checkNull: true })
         .isString()
         .isLength({
@@ -55,6 +63,11 @@ forgotRoute.post('/reset', [
                  path.join(__dirname, '../../public', 'reset_password_result_error.html'),
                  { errors: errors.array() })
         } else {
+            // req.query.token
+            // req.query.new_password
+
+
+
             return res.render(
                 path.join(__dirname, '../../public', 'reset_password_result_success.html'))
         }            
