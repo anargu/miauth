501d90d6d48708a927c86b2850c4a4b7265e8aff
Anargu <anthony.arostegui@gmail.com>
2020-01-17 21:03:29 -0500

    extending variables by miauthConfig

--- src/config/index.js
+++ src/config/index.js
@@ -4,11 +4,25 @@ const fs = require('fs')
 let miauthConfig = null
 
 function initConfig() {
-    const { MIAUTH_CONFIG_FILE, PORT } = process.env
-    const miauthConfigString = fs.readFileSync(MIAUTH_CONFIG_FILE, { encoding: 'utf-8' })
+    const { 
+        MIAUTH_CONFIG_FILE,
+        PORT,
+        SALT,
+        JWT_EXP_OFFSET,
+        JWT_SECRET,
+        REFRESH_SECRET
+    } = process.env
+    const miauthConfigString = fs.readFileSync(MIAUTH_CONFIG_FILE, { encoding: 'utf-8' })    
 
     miauthConfig = YAML.parse(miauthConfigString)
     miauthConfig['PORT'] = PORT
+    
+    miauthConfig['bcrypt'] = {}
+    miauthConfig.bcrypt.SALT = SALT
+    
+    miauthConfig['JWT_EXP_OFFSET'] = JWT_EXP_OFFSET
+    miauthConfig['JWT_SECRET'] = JWT_SECRET
+    miauthConfig['REFRESH_SECRET'] = REFRESH_SECRET
 
     return miauthConfig
 }
