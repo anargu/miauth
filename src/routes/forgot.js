91a5783bc9c6e4246c039e4576132e9a2dc61a26
Anargu <anthony.arostegui@gmail.com>
2020-01-18 13:37:00 -0500

    forgot pass WIP

--- src/routes/forgot.js
+++ src/routes/forgot.js
@@ -4,23 +4,63 @@ const express = require('express')
 const { check, oneOf, validationResult, query } = require('express-validator');
 const miauthConfig = require('../config')
 const { MiauthError } = require('../utils/error.js')
+const { User, Session } = require('../models')
+const { tokenize, expirationOffset } = require('../utils/token')
 
 const forgotRoute = express.Router()
 
 // step 1: user request for reset password
-forgotRoute.post('/request', [
+forgotRoute.post('/request', oneOf([
     check('username', miauthConfig.field_validations.username.invalid_pattern_error_message)
-    .exists({ checkNull: true })
-    .matches(new RegExp(miauthConfig.field_validations.username.pattern, 'g'))
-    .isLength({
-        min: miauthConfig.field_validations.username.len[0],
-        max: miauthConfig.field_validations.username.len[1]
-    }).withMessage(`Invalid username. Username should be between\ 
-    ${miauthConfig.field_validations.username.len[0]} and\ 
-    ${miauthConfig.field_validations.username.len[1]} characteres`)
-], () => {
+        .exists({ checkNull: true })
+        .matches(new RegExp(miauthConfig.field_validations.username.pattern, 'g'))
+        .isLength({
+            min: miauthConfig.field_validations.username.len[0],
+            max: miauthConfig.field_validations.username.len[1]
+        }).withMessage(`Invalid username. Username should be between\ 
+        ${miauthConfig.field_validations.username.len[0]} and\ 
+        ${miauthConfig.field_validations.username.len[1]} characteres`),
+    check('email', miauthConfig.field_validations.email.invalid_pattern_error_message)
+        .exists({ checkNull: true })
+        .matches(new RegExp(miauthConfig.field_validations.email.pattern, 'g'))
+        .isLength({
+            min: miauthConfig.field_validations.email.len[0],
+            max: miauthConfig.field_validations.email.len[1]
+        }).withMessage(`Invalid email. Email should be between\ 
+        ${miauthConfig.field_validations.email.len[0]} and\ 
+        ${miauthConfig.field_validations.email.len[1]} characteres`)
+]), async (req, res) => {
+    try {
+        validationResult(req).throw()
 
-    // 
+        const fieldValue = req.body.username || req.body.email
+        const findByUsername = req.body.username === undefined ? false : true
+        
+        let _userFound
+        if (findByUsername) {
+            _userFound = await User.findByUsername(fieldValue)
+        } else {
+            _userFound = await User.findByEmail(fieldValue)
+        }
+        if (!_userFound)
+            throw new MiauthError(400, 'user_not_found', 'Usuario no encontrado/registrado.')
+        // user found, generate token
+        const resetEmailToken = await tokenize(
+            { userId: _userFound.uuid, email: _userFound.email },
+            miauthConfig.reset_password.secret,
+            expirationOffset(miauthConfig.reset_password.token_expiration),
+            
+        )
+        // send email to user
+        // TODO: Create Email Service
+
+        // respond ok
+        res.status(200).json({
+            message: 'Email sent to the user with the instructure to restore password.'
+        })
+    } catch (err) {
+        throw err
+    }
 })
 
 forgotRoute.get('/reset', (req, res) => {
