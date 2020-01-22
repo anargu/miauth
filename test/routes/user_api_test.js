f3099bf197bffa31362d3bfd8062514c53f6bcd9
Anargu <anthony.arostegui@gmail.com>
2020-01-21 21:37:58 -0500

    updated tests

--- test/routes/user_api_test.js
+++ test/routes/user_api_test.js
@@ -1,5 +1,4 @@
 const assert = require('assert')
-const path = require('path')
 const chaiHttp = require("chai-http")
 const chai = require('chai')
 
@@ -8,76 +7,25 @@ const express = require('express')
 chai.should()
 chai.use(chaiHttp)
 
-const { checkSchema, validationResult } = require('express-validator')
+const { validationResult, oneOf } = require('express-validator')
 
 describe('User Api test methods', () => {
 
     let app
     before('setting env variables', () => {
         const miauthConfig = require('../utils/init_config').miauthSetup()
-
-        const userSchemaValidation = (() => {
-            const _userSchemaValidation = {}
-            if (miauthConfig.user.username) {
-                _userSchemaValidation['username'] = {
-                    matches: {
-                        options: [new RegExp(miauthConfig.field_validations.username.pattern), 'g'],
-                    },
-                    isLength: {
-                        errorMessage: 'Invalid username. Username should be between ' +
-                            miauthConfig.field_validations.username.len[0] + ' and ' +
-                            miauthConfig.field_validations.username.len[1] +  ' characteres.',
-                        options: {
-                            min: miauthConfig.field_validations.username.len[0],
-                            max: miauthConfig.field_validations.username.len[1],
-                        }
-                    },
-                    notEmpty: true,
-                    errorMessage: miauthConfig.field_validations.username.invalid_pattern_error_message,
-                }
-            }
-            
-            if (miauthConfig.user.email) {
-                _userSchemaValidation['email'] = {
-                    matches: {
-                        options: [new RegExp(miauthConfig.field_validations.email.pattern), 'g'],
-                    },
-                    isLength: {
-                        errorMessage: 'Invalid email. Email should be between ' +
-                        miauthConfig.field_validations.email.len[0] + 'and' +
-                        miauthConfig.field_validations.email.len[1] + ' characteres.',
-                        options: {
-                            min: miauthConfig.field_validations.email.len[0],
-                            max: miauthConfig.field_validations.email.len[1],
-                        }
-                    },
-                    errorMessage: miauthConfig.field_validations.email.invalid_pattern_error_message,
-                    notEmpty: true,
-                }
-            }
-
-            _userSchemaValidation['password'] = {
-                notEmpty: true,
-                isString: true,
-                isLength: {
-                    options: {
-                        min: miauthConfig.field_validations.password.len[0],
-                        max: miauthConfig.field_validations.password.len[1],
-                    },
-                    errorMessage: 'Invalid password. Password should be between ' +
-                        miauthConfig.field_validations.password.len[0] + ' and ' +
-                        miauthConfig.field_validations.password.len[1] + ' characteres.',
-                },
-                errorMessage: miauthConfig.field_validations.password.invalid_pattern_error_message,
-            }
-
-            return _userSchemaValidation
-        })()
+        const { check_username, check_email, check_password } = require('../../src/middlewares/validations')
 
         app = express()
         app.use(express.json())
         app.use(express.urlencoded({ extended: true }))
-        app.post('/login', checkSchema(userSchemaValidation), async (req,res) => {
+        app.post('/login', [
+            check_password(),
+            oneOf([
+                ... (miauthConfig.user.username) ? [check_username()] : [],
+                ... (miauthConfig.user.email) ? [check_email()] : [],
+            ])
+        ], async (req,res) => {
             if (!validationResult(req).isEmpty()) {
                 // console.log(validationResult(req).errors)
                 res.status(400).send({
@@ -88,12 +36,16 @@ describe('User Api test methods', () => {
             res.status(200).send({})
         })
 
-        app.post('/signup', checkSchema(userSchemaValidation), (req,res) => {
+        app.post('/signup', [
+            ... (miauthConfig.user.username) ? [check_username()] : [],
+            ... (miauthConfig.user.check_email) ? [check_email()] : [],
+            check_password(),
+        ], (req,res) => {
             res.status(200).send({})
         })
     });
 
-    describe('When User logins [Chai]', () => {
+    describe('When User logins ', () => {
         const testcases = [
             { username: 'Juan1234', email: 'abc@hotmail.com', password: 'juan' },
             { username: 'Juanabc', email: 'absdsc@hotmail.com', password: 'juan1' },
