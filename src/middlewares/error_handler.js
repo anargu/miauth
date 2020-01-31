b3f9a1b77d74d3defc49aa25f98ef7c0d292b3c6
Anargu <anthony.arostegui@gmail.com>
2020-01-30 22:09:30 -0500

    updating error handler

--- src/middlewares/error_handler.js
+++ src/middlewares/error_handler.js
@@ -7,10 +7,19 @@ const handleError = (err, req, res, next) => {
         const { code, message, field } = err
         res.status(400).json({
             code,
+            error: 'MulterError',
             error_description: message,
             user_message: `${field}: ${message}`
         })
         return    
+    } else if(err.isAxiosError) {
+        res.status(500).json({
+            code: 0,
+            error: 'DOSMJError',
+            error_description: err.response.data.error,
+            user_message: 'Mail service not working properly. Please try later.'
+        })
+        return
     }
     const { statusCode, error, message, user_message, name } = err
     res.status(statusCode || 500).json({
